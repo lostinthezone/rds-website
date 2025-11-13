b8f5eb1fe4e8586ebb5318ea162b14f5c0c6d28a
lostinthezone <russellshackel@hotmail.com>
2025-11-13 22:38:58 +1000

    Update layout

--- app/layouts/app-layout.vue
+++ app/layouts/app-layout.vue
@@ -1,33 +1,60 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <div class="border-b-1 border-gray-200">
-    <header class="container mx-auto flex justify-between items-center pt-3 pb-3">
-      <div>
-        <NuxtLink to="/">
-          <img src="/rds-long-logo.png" alt="RDS logo" class="max-w-xs"/>
-        </NuxtLink>
-      </div>
-      <nav>
-        <ul class="flex space-x-10 text-xl">
-          <li><NuxtLink to="/anydeck">AnyDeck</NuxtLink></li>
-          <li><NuxtLink to="/contact">Contact</NuxtLink></li>
-        </ul>
-      </nav>
-    </header>
-  </div>
+  <UHeader :toggle="false">
+    <template #left>
+      <NuxtLink to="/">
+        <img src="/rds-long-logo.png" alt="RDS logo" class="max-w-xs h-10"/>
+      </NuxtLink>
+    </template>
+    
+    <template #right>
+      <ul class="flex space-x-10 text-xl items-center">
+        <li><NuxtLink to="/anydeck">AnyDeck</NuxtLink></li>
+        <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+        <li><UColorModeButton /></li>
+      </ul>
+    </template>
+  </UHeader>
   
-  <div class="container mx-auto">
-    <slot/>
-  </div>
+  <UMain>
+    <UContainer>
+      <slot />
+    </UContainer>
+  </UMain>
+  
+  <UFooter>
+    <template #left>
+      <span>Copyright @ 2025</span>
+    </template>
+  </UFooter>
+  
+<!--  <div class="border-b-1 border-gray-200">-->
+<!--    <header class="container mx-auto flex justify-between items-center pt-3 pb-3">-->
+<!--      <div>-->
+<!--        <NuxtLink to="/">-->
+<!--          <img src="/rds-long-logo.png" alt="RDS logo" class="max-w-xs"/>-->
+<!--        </NuxtLink>-->
+<!--      </div>-->
+<!--      <nav>-->
+<!--        <ul class="flex space-x-10 text-xl">-->
+<!--          <li><NuxtLink to="/anydeck">AnyDeck</NuxtLink></li>-->
+<!--          <li><NuxtLink to="/contact">Contact</NuxtLink></li>-->
+<!--        </ul>-->
+<!--      </nav>-->
+<!--    </header>-->
+<!--  </div>-->
+<!--  -->
+<!--  <div class="container mx-auto">-->
+<!--    <slot/>-->
+<!--  </div>-->
 
-  <div class="border-t-1 border-gray-200 mt-10">
-    <div class="container mx-auto pt-8 pb-8">
-      Copyright @ 2025  
-    </div>
-  </div>
+<!--  <div class="border-t-1 border-gray-200 mt-10">-->
+<!--    <div class="container mx-auto pt-8 pb-8">-->
+<!--      Copyright @ 2025  -->
+<!--    </div>-->
+<!--  </div>-->
   
 </template>
 
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,26 +1,29 @@
 <script setup lang="ts">
-  useTrackEvent("contact_event", {
-    when: new Date().toUTCString(),
-    page: 'contact'
-  });
+useTrackEvent("contact_event", {
+  when: new Date().toUTCString(),
+  page: 'contact'
+});
 
-  useHead({
-    title: 'Really Digital Stuff - Contact',
-    meta: [
-      { name: 'description', content: 'Contact Really Digital Stuff' }
-    ]
-  })  
+useHead({
+  title: 'Really Digital Stuff - Contact',
+  meta: [
+    {name: 'description', content: 'Contact Really Digital Stuff'}
+  ]
+})
 </script>
 
 <template>
   <div class="pt-8">
-  <div class="flex content-center justify-center">
-    <img src="/liame.png" alt="Anydeck logo" class="max-w-56"/>
-  </div>
-  
-  <div class="pt-8">
-    reallydigitalstuff at gmail.com 
-  </div>
+    <div class="flex content-center justify-center">
+      <div class="flex flex-col items-center">
+        <img src="/liame.png" alt="Anydeck logo" class="max-w-56"/>
+
+        <div class="pt-2">
+          reallydigitalstuff at gmail dot com
+        </div>        
+      </div>
+
+    </div>
   </div>
 </template>
 
