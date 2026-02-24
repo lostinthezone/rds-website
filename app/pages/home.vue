a6424a7a492214fb9ff5dd0bf1769059f6a145ee
lostinthezone <russellshackel@hotmail.com>
2026-02-24 21:52:09 +1000

    Add games

---
+++ app/pages/home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex content-center justify-center pt-8">
+    <img src="/loading.png" alt="Loading" class="h-80"/>
+  </div>
+</template>
